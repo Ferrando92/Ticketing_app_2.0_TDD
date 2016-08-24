3e0d0e79ba261d4b43e71b26fcbecf2c2349d36a
Sergio Ferrando López <Sergio@iMac-de-Sergio.local>
2016-08-24 15:16:53 +0200

    stuff from views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   get '/' do
-  		#default me muestra layout, con esto me muestra lo que le digo, why?
-  	   erb :welcome_message, :layout => :welcome_message
+  	   
+  	   erb :welcome_message
+  	   
   end
 end
